6654ddf9ee7c01a6d9b97d5ad92ff3b44a2d7049
Allen Mordukhaev <allenmor93@gmail.com>
2022-09-10 13:23:01 -0400

    commit tree 937bd39739caee8ed10133ff34d7e81687cce712

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,27 @@
 class ApplicationController < Sinatra::Base
-
+  set :default_content_type, 'application/json'
   # add routes
+  get '/bakeries' do
+    bakeries = Bakery.all
 
+    bakeries.to_json
+
+  end
+
+  get '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+
+    bakery.to_json(include: :baked_goods)
+  end
+
+  get '/baked_goods/by_price' do
+    descending_prices = BakedGood.order(price: :desc)
+
+    descending_prices.to_json
+  end
+  get '/baked_goods/most_expensive' do
+    most = BakedGood.order(price: :desc).first
+
+    most.to_json
+  end
 end
